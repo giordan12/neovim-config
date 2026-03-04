07dc6579afb156bfb4d22f78d3885589c02f9996
Gio Castro <giordan12@me.com>
2026-03-03 22:10:14 -0600

    Leveraging lazy extras

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = args.buf, desc = "LSP Hover" })
+  end,
+})
